ae463293a137666cab59a02644dfb3b417f87c9f
Rafichera <raficho.exe@gmail.com>
2025-10-20 23:04:49 -0300

    update

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -11,10 +11,12 @@ import { Header } from "../../components/header/header";
 export class Home {
   index = 0;
   total = 3; // número de imagens
-  slides = 'translateX(0%)';
 
   moverSlide(direcao: number) {
     this.index = (this.index + direcao + this.total) % this.total;
-    this.slides = `translateX(${-this.index * 100}%)`;
+  }
+
+  getTransform() {
+    return `translateX(${-this.index * 100}%)`;
   }
 }
