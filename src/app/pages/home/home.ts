b6eb8eb611897b1113cb6e7f061bff1cb63feede
dieguin0019 <diegojinunjoses@gmail.com>
2025-11-03 15:36:28 -0300

    tentativa de falsh card

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { Header } from "../../components/header/header";
 import { Slides } from '../../components/slides/slides';
 import { Hero } from '../../components/hero/hero';
 import { FrasesSlider } from "../../components/frases-slider/frases-slider";
+import { Flashcards } from '../../components/flashcards/flashcards';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Slides, Hero, FrasesSlider],
+  imports: [Header, Slides, Hero, FrasesSlider,Flashcards],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
